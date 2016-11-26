ca38e9aaeaf677ddae19f0af64b6fd1139368258
Daniel Lerps <d@niel.lerps.de>
2016-11-26 18:11:55 +0100

    Restructured & switched to Sass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "locksmith-root",
+  templateUrl: "./app.component.html",
+  styleUrls: [
+    "./app.component.scss"
+    ]
 })
 export class AppComponent {
   title = 'Password Generator';
